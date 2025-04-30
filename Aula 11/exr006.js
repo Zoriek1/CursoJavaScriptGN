e698b928a5d32c35f15d0a8309844c18bbaddcc8
Zoriek <caionixt@gmail.com>
2025-04-30 12:17:45 -0300

    Commit 30/04 Até aula 12

---
+++ Aula 11/exr006.js
@@ -0,0 +1,9 @@
+var Pais = 'Brasil'
+console.log(`Sua nacionalidade é: ${Pais}`)
+if (Pais != 'Brasil'){
+    console.log('Você é Estrangeiro')
+}
+else {
+    console.log('Você é brasileiro')
+}
+console.log('Boa viagem!')
---
+++ Aula 12/exr009.js
@@ -0,0 +1,15 @@
+var agora = new Date
+var hora = agora.getHours()
+
+console.log(`agora são exatamente ${hora} horas`)
+
+if (hora < 12 && hora > 4 ){
+    console.log("ainda está de manhã, BOM DIA!")
+}
+    else if (hora >= 12 && hora < 18){
+        console.log("já está de tarde, BOA TARDE!")
+}
+        else {
+            console.log("Já anoiteceu, BOA NOITE!")
+}
+            
