ee0c9ea0c6b8f11bf13b8f19326537eb5b1b41db
Zoriek <caionixt@gmail.com>
2025-05-13 14:27:45 -0300

    Commit Exercicios pt 2

--- Aula 12 ex/Exr010/Script.js
+++ Aula 12 ex/Exr010/Script.js
@@ -1,1 +1,25 @@
-alert("Olá")
+
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora}h `
+    
+    if (hora >= 5 && hora < 12){
+        //Bom dia
+        img.src = 'Foto manhã.png'
+        document.body.style.backgroundColor = "#83c6f1"
+    }
+    else if (hora >= 12 && hora< 19)  {
+        // Boa tarde
+        img.src = 'Foto Tarde.png'
+        document.body.style.backgroundColor = "#bf9b7d"
+    }
+    else {
+        // Boa noite
+        img.src = 'Foto noite.png'
+        document.body.style.backgroundColor = "#303455"
+    }
+
+}
